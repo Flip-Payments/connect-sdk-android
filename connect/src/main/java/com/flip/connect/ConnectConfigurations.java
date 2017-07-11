0d101ecd8866f28af86a0da24536dbf672799798
Caique Oliveira <j.caique_fla@hotmail.com>
2017-07-11 14:01:24 -0300

    update initialization of connect

---
+++ connect/src/main/java/com/flip/connect/ConnectConfigurations.java
@@ -0,0 +1,61 @@
+package com.flip.connect;
+
+/**
+ * Created by Kanda on 11/07/2017.
+ */
+
+public class ConnectConfigurations {
+    private String clientId;
+    private String host;
+    private String schema;
+    private String clientSecret;
+    private String publicToken;
+
+    String getClientId() {
+        if (clientId == null)
+            throw new RuntimeException("clientId not defined");
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    protected String getHost() {
+        if (host == null)
+            throw new RuntimeException("Host not defined");
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+     String getSchema() {
+        if (schema == null)
+            throw new RuntimeException("Schema not defined");
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+     String getClientSecret() {
+        if (schema == null)
+            throw new RuntimeException("ClientSecret not defined");
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    protected String getPublicToken() {
+        return publicToken;
+    }
+
+    public void setPublicToken(String publicToken) {
+        this.publicToken = publicToken;
+    }
+}
